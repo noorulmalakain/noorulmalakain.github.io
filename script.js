c1679ddb36157f6018222f42212c3d209992b24f
shaheer hussain <shaheer@aviatrix.com>
2024-05-04 21:13:15 -0600

    update files

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // script.js
+var backend_url = "https://rtlqdhy3r7kmbxdblsl32pu6ta0oelez.lambda-url.us-west-2.on.aws/";
+var APP_VERSION = "0.0.1"
 $(document).ready(function(){
-  console.log("version: 0.01");
+  console.log("version: " + APP_VERSION);
+  $("#appVersion").text(APP_VERSION);
   // Function to make a GET request to the API and update the paragraph tag
   function callAPI() {
-      $.get("https://api.example.com/data", function(data, status){
-          $("#apiResponse").text(data);
-      });
+    $.get(backend_url, function(data, status){
+      $("#apiResponse").text(data);
+    });
   }
 
   // Event listener for the button click
   $("#fetchButton").click(function(){
-      callAPI();
+    callAPI();
   });
 });
